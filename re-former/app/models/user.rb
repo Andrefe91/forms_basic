39de91604f75e46698f636a0ec0a54b27caca5e4
Andres Felipe -Main <andrefe91@gmail.com>
2024-01-22 23:34:04 -0500

    Functioning HTML only form

--- re-former/app/models/user.rb
+++ re-former/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
-  validates :username, :email, :password, presence: true
+  validates :username, :email, :password, presence: true, allow_blank: false
 
   validates :username, length: {in: 5..25}, uniqueness: true
   validates :email, length: {in: 5..40}, uniqueness: true
